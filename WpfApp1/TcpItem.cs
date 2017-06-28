18714fab4b72664c5552d8dc7a2e283c4b650e70
Ali Khalili <ali.khll.1994@gmail.com>
2017-06-28 12:35:50 +0430

    tcp client enhanced

---
+++ WpfApp1/TcpItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    public class TcpItem
+    {
+        public NetworkStream Initial(string ip, int port = 23)
+        {
+            TcpClient server = new TcpClient(ip, port);
+            return server.GetStream(); //ethernet
+        }
+    }
+}
