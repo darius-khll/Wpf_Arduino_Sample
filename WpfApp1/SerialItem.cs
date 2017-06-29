91b7833b45e097a529005c81c2cfcf7cc30e35bf
Ali Khalili <ali.khll.1994@gmail.com>
2017-06-29 15:46:25 +0430

    serial service fixed

---
+++ WpfApp1/SerialItem.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    public class SerialItem
+    {
+        public SerialPort Serial(string portTxt)
+        {
+            SerialPort mySerialPort = null;
+            int count = 0;
+            Line:
+            try
+            {
+                string port = portTxt;
+                mySerialPort = new SerialPort(port);
+
+                mySerialPort.BaudRate = 9600;
+                mySerialPort.Parity = Parity.None;
+                mySerialPort.StopBits = StopBits.One;
+                mySerialPort.DataBits = 8;
+                mySerialPort.Handshake = Handshake.None;
+
+                mySerialPort.Open();
+
+            }
+            catch (Exception e)
+            {
+                if (count == 5)
+                {
+                    throw new Exception("Error");
+                }
+                count++;
+                goto Line;
+            }
+            return mySerialPort;
+        }
+    }
+}
